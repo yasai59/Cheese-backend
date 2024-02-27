7e1e93f15aacb8f2223948ad816a4e246c1301a7
Yasai <alexvidalcasado@gmail.com>
2024-02-27 10:23:09 +0100

    [FIX] mysql disconnection

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,9 +1,24 @@
 import mysql from "mysql2";
 import dbConfig from "./db.config";
 
-export default mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASS,
-  database: dbConfig.NAME,
-});
+let connection;
+
+const handleDisconnect = () => {
+  connection = mysql.createPool({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASS,
+    database: dbConfig.NAME,
+  });
+
+  connection.on("error", (err) => {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+};
+
+export default connection;

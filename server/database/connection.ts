c79f2cd8e0f625c568125e84d966ed66c0f3380e
Yasai <alexvidalcasado@gmail.com>
2024-02-27 10:27:04 +0100

    [FIX] db

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -4,11 +4,18 @@ import dbConfig from "./db.config";
 let connection;
 
 const handleDisconnect = () => {
-  connection = mysql.createPool({
+  connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASS,
     database: dbConfig.NAME,
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("Error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
   });
 
   connection.on("error", (err) => {

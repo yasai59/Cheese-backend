c3ad4b9a9b63becda3ee3910b97cb3a54f6962c0
Yasai <alexvidalcasado@gmail.com>
2024-02-29 12:20:29 +0100

    [FIX] duplicating entries when updating user tastes

--- server/database/Taste.repository.ts
+++ server/database/Taste.repository.ts
@@ -11,7 +11,7 @@ interface ITasteRepository {
 export default class TasteRepository implements ITasteRepository {
   public async findUserTastes(userId: number): Promise<TasteModel[]> {
     const query =
-      "SELECT t.name FROM taste t JOIN user_taste ut ON t.id = ut.taste_id WHERE ut.user_id = ?";
+      "SELECT t.name, t.id FROM taste t JOIN user_taste ut ON t.id = ut.taste_id WHERE ut.user_id = ?";
     try {
       const result = await connection.promise().query(query, [userId]);
       const tastes: RowDataPacket[] = result[0] as RowDataPacket[];
@@ -27,11 +27,16 @@ export default class TasteRepository implements ITasteRepository {
   ): Promise<TasteModel[]> {
     const query = "INSERT INTO user_taste (user_id, taste_id) VALUES (?, ?)";
     try {
-      for (let i = 0; i < tastes.length; i++) {
-        await connection.promise().query(query, [userId, tastes[i]]);
+      connection
+        .promise()
+        .query("DELETE FROM user_taste WHERE user_id = ?", [userId]);
+
+      for (const taste of tastes) {
+        await connection.promise().query(query, [userId, taste]);
       }
       return await this.findUserTastes(userId);
     } catch (error) {
+      console.log(error);
       throw new Error("Error adding tastes to user");
     }
   }

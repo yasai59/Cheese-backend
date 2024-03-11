c563ef66cab4af9282c59875182223253ecbb254
ariosbuil2002 <arribu2002@gmail.com>
2024-03-11 10:00:46 +0100

    [ADD] Photos Management

--- server/database/Restaurant.repository.ts
+++ server/database/Restaurant.repository.ts
@@ -1,21 +1,22 @@
 import connection from "./connection";
 import type RestaurantModel from "../models/Restaurant.model";
 import type { RowDataPacket } from "mysql2";
+import type User from "../models/User.model";
 
 interface IRestaurantRepository {
-    save(restaurant: RestaurantModel): Promise<RestaurantModel>;
-    findByOwner(ownerId: number): Promise<RestaurantModel[]>;
-    findFavoriteRestaurants(userId: number): Promise<RestaurantModel[]>;
-    addFavoriteRestaurant(userId: number, restaurantId: number): Promise<void>;
-    findLikedRestaurants(userId: number): Promise<RestaurantModel[]>;
-    addLikedRestaurant(userId: number, restaurantId: number): Promise<void>;
+    save(restaurant: RestaurantModel, user: User): Promise<RestaurantModel>;
+    findByOwner(user: User): Promise<RestaurantModel[]>;
+    findFavoriteRestaurants(user: User): Promise<RestaurantModel[]>;
+    addFavoriteRestaurant(user: User, restaurantId: number): Promise<void>;
+    findLikedRestaurants(user: User): Promise<RestaurantModel[]>;
+    addLikedRestaurant(user: User, restaurantId: number): Promise<void>;
 }
 
 export default class RestaurantRepository implements IRestaurantRepository {
-    public async save(restaurant: RestaurantModel): Promise<RestaurantModel> {
+    public async save(restaurant: RestaurantModel, user: User): Promise<RestaurantModel> {
         const query = "INSERT INTO restaurant (name, owner_id, address, phone) VALUES (?, ?, ?, ?)";
         try {
-            const result = await connection.promise().query(query, [restaurant.name, restaurant.owner_id, restaurant.address, restaurant.phone]);
+            const result = await connection.promise().query(query, [restaurant.name, user.user_id, restaurant.address, restaurant.phone]);
             const restaurants: RowDataPacket[] = result[0] as RowDataPacket[];
             const restaurantSaved = restaurants[0] as RestaurantModel;
             return restaurantSaved;
@@ -24,10 +25,10 @@ export default class RestaurantRepository implements IRestaurantRepository {
         } 
     }
 
-    public async findByOwner(ownerId: number): Promise<RestaurantModel[]> {
+    public async findByOwner(user: User): Promise<RestaurantModel[]> {
         const query = "SELECT * FROM restaurant WHERE owner_id = ?";
         try {
-            const result = await connection.promise().query(query, [ownerId]);
+            const result = await connection.promise().query(query, [user.user_id]);
             const restaurants: RowDataPacket[] = result[0] as RowDataPacket[];
             return restaurants as RestaurantModel[];
         } catch (error) {
@@ -35,10 +36,10 @@ export default class RestaurantRepository implements IRestaurantRepository {
         }    
     }
 
-    public async findFavoriteRestaurants(userId: number): Promise<RestaurantModel[]> {
+    public async findFavoriteRestaurants(user: User): Promise<RestaurantModel[]> {
         const query = "SELECT * FROM restaurant WHERE id IN (SELECT restaurant_id FROM favorite_restaurant WHERE user_id = ?)";
         try {
-            const result = await connection.promise().query(query, [userId]);
+            const result = await connection.promise().query(query, [user.user_id]);
             const restaurants: RowDataPacket[] = result[0] as RowDataPacket[];
             return restaurants as RestaurantModel[];
         } catch (error) {
@@ -46,19 +47,19 @@ export default class RestaurantRepository implements IRestaurantRepository {
         }
     }
 
-    public async addFavoriteRestaurant(userId: number, restaurantId: number): Promise<void> {
+    public async addFavoriteRestaurant(user: User, restaurantId: number): Promise<void> {
         const query = "INSERT INTO favorite_restaurant (user_id, restaurant_id) VALUES (?, ?)";
         try {
-            await connection.promise().query(query, [userId, restaurantId]);
+            await connection.promise().query(query, [user.user_id, restaurantId]);
         } catch (error) {
             throw new Error("Error adding favorite restaurant");
         }
     }
 
-    public async findLikedRestaurants(userId: number): Promise<RestaurantModel[]> {
+    public async findLikedRestaurants(user: User): Promise<RestaurantModel[]> {
         const query = "SELECT * FROM restaurant WHERE id IN (SELECT restaurant_id FROM liked_restaurant WHERE user_id = ?)";
         try {
-            const result = await connection.promise().query(query, [userId]);
+            const result = await connection.promise().query(query, [user.user_id]);
             const restaurants: RowDataPacket[] = result[0] as RowDataPacket[];
             return restaurants as RestaurantModel[];
         } catch (error) {
@@ -66,12 +67,13 @@ export default class RestaurantRepository implements IRestaurantRepository {
         }
     }
 
-    public async addLikedRestaurant(userId: number, restaurantId: number): Promise<void> {
+    public async addLikedRestaurant(user: User, restaurantId: number): Promise<void> {
         const query = "INSERT INTO liked_restaurant (user_id, restaurant_id) VALUES (?, ?)";
         try {
-            await connection.promise().query(query, [userId, restaurantId]);
+            await connection.promise().query(query, [user.user_id, restaurantId]);
         } catch (error) {
             throw new Error("Error adding liked restaurant");
         }
     }
+
 }
--- server/routes/restaurantRoutes.ts
+++ server/routes/restaurantRoutes.ts
@@ -2,95 +2,267 @@ import express, { Router } from "express";
 import type { Request, Response } from "express";
 import { check } from "express-validator";
 import { validarCampos } from "../helpers/verifyFields";
-
 import RestaurantRepository from "../database/Restaurant.repository";
 import type RestaurantModel from "../models/Restaurant.model";
+import type User from "../models/User.model";
+import { verifyJWT } from "../middlewares/verifyJWT";
+import multer from "multer";
+import path from "path";
+import { v4 as uuidv4 } from 'uuid';
+import connection from "../database/connection";
+import fs from "fs";
+
+interface PhotoRequest extends Request {
+    photoName?: string | Array<string>;
+}
 
 const restaurantRepository = new RestaurantRepository();
 
 const restaurantRouter: Router = express.Router();
 
+const dir = path.dirname(new URL(import.meta.url).pathname);
+
+const storageCarousel = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(dir, "../../uploads/restaurant_photos/carousel/"));
+    },
+    filename: (req: PhotoRequest, file, cb) => {
+        const name = req.user?.name + "_" + uuidv4()+ Date.now + "." + path.extname(file.originalname);
+        if(!req.photoName){
+            req.photoName = [];
+        }
+        (req.photoName as Array<string>).push(name);
+        cb(null, name);
+    }
+});
+
+const storageProfilePicture = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(dir, "../../uploads/restaurant_photos/profile_pictures/"));
+    },
+    filename: (req: PhotoRequest, file, cb) => {
+        const name = req.user?.username + "_" + uuidv4() + "." + path.extname(file.originalname);
+        req.photoName = name;
+        cb(null, name);
+    }
+});
+
+const uploadCarousel = multer({ storage: storageCarousel });
+const uploadProfilePicture = multer({ storage: storageProfilePicture });
+
+// GET /api/restaurant/photo/carousel
+
+
+// POST /api/restaurant/photo/carousel
+restaurantRouter.post(
+    '/photo/carousel',
+    [
+        // verifyJWT,
+        uploadCarousel.array("photo", 12)
+    ],
+    async (req: PhotoRequest, res: Response) => {
+        try {
+            const photos = req.photoName as Array<string>;
+            if (!photos) {
+                return res.status(400).json({ message: "No files uploaded" });
+            }
+            const restaurant = req.body as RestaurantModel;
+            if (!restaurant.id) {
+                return res.status(400).json({ message: "Restaurant id is required" });
+            }
+            const query = "INSERT INTO carousel (restaurant_id, photo) VALUES (?, ?)";
+            photos.forEach(photo => {
+                connection.query(query, [restaurant.id, photo], (err) => {
+                    if (err) {
+                        return res.status(500).json({ message: "Error saving photo in database" });
+                    }
+                });
+            });
+            return res.status(201).json({ message: "Photos uploaded successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Error uploading photos" });
+        }
+    }
+)
+
+// GET /api/restaurant/photo/profile-picture
+restaurantRouter.get(
+    "/photo/profile-picture",
+    // [
+    //     verifyJWT
+    // ],
+    async (req: Request, res: Response) => {
+        let restaurant = req.body;
+        if (!restaurant) {
+            return res.sendFile(
+                path.join(dir, "../../uploads/restaurant_photos/profile_pictures/default.jpg")
+            );
+        }
+        try {
+            if (!fs.existsSync(path.join(dir, "../../uploads/restaurant_photos/profile_pictures/"))) {
+                return res.sendFile(
+                    path.join(dir, "../../uploads/restaurant_photos/profile_pictures/default.jpg")
+                );
+            }
+            res.sendFile(path.join(dir, "../../uploads/restaurant_photos/profile_pictures/" + restaurant.photo));
+            return res.status(201).json({ message: "Profile picture uploaded successfully", photo: restaurant.photo });
+        } catch (error) {
+            res.status(500).json({ message: "Error uploading profile picture" });
+        }
+    });
+
+// POST /api/restaurant/photo/profile-picture
+restaurantRouter.post(
+    "/photo/profile-picture",
+    [
+        // verifyJWT,
+        uploadProfilePicture.single("photo")
+    ],
+    async (req: PhotoRequest, res: Response) => {
+        try {
+            const photo = req.photoName;
+            let restaurant = req.body as RestaurantModel;
+            if (!photo) {
+                return res.status(400).json({ message: "No file uploaded" });
+            }
+            if (!restaurant.id) {
+                return res.status(400).json({ message: "Restaurant id is required" });
+            }
+            const query = "UPDATE restaurant SET photo = ? WHERE id = ?";
+            connection.query(query, [photo, restaurant?.id], (err) => {
+                if (err) {
+                    return res.status(500).json({ message: "Error saving photo in database" });
+                }
+                return res.status(201).json({ message: "Profile picture uploaded successfully" });
+            });
+        } catch (error) {
+            res.status(500).json({ message: "Error uploading profile picture" });
+        }
+    });
+
 // POST api/restaurant
 restaurantRouter.post(
-"/", 
-[
-    check("name", "Name is required").not().isEmpty(),
-    validarCampos
-]
-,async (req: Request, res: Response) => {
-    const restaurant: RestaurantModel = req.body;
-    try {
-        const restaurantSaved = await restaurantRepository.save(restaurant);
-        res.status(201).json({
-            message: "Restaurant created successfully",
-            restaurantSaved
-        });
-    } catch (error) {
-        res.status(500).json({ message: "Error creating the restaurant" });
-    }
-});
-
-
-// GET api/restaurant/:ownerId
-restaurantRouter.get("/:ownerId", async (req: Request, res: Response) => {
-    const ownerId: number = parseInt(req.params.ownerId);
-    try {
-        const restaurants = await restaurantRepository.findByOwner(ownerId);
-        res.status(200).json(restaurants);
-    } catch (error) {
-        res.status(500).json({ message: "Error finding restaurants by owner" });
-    }
-});
-
-
-// GET api/restaurant/:userId/favorite-restaurants
-restaurantRouter.get("/:userId/favorite-restaurants", async (req: Request, res: Response) => {
-    const userId: number = parseInt(req.params.userId);
-    try {
-        const favoriteRestaurants = await restaurantRepository.findFavoriteRestaurants(userId);
-        res.status(200).json(favoriteRestaurants);
-    } catch (error) {
-        res.status(500).json({ message: "Error finding favorite restaurants by user" });
-    }
-})
-
-// POST api/restaurant/:userId/favorite-restaurant/:restaurantId
-restaurantRouter.post("/:userId/favorite-restaurant/:restaurantId", async (req: Request, res: Response) => {
-    const userId: number = parseInt(req.params.userId);
-    const restaurantId: number = parseInt(req.params.restaurantId);
-    try {
-        await restaurantRepository.addFavoriteRestaurant(userId, restaurantId);
-        res.status(200).json({ message: "Favorite restaurant added successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Error adding favorite restaurant" });
-    }
-});
-
-
-// GET api/restaurant/:userId/liked-restaurants
-restaurantRouter.get("/:userId/liked-restaurants", async (req: Request, res: Response) => {
-    const userId: number = parseInt(req.params.userId);
-    try {
-        const likedRestaurants = await restaurantRepository.findLikedRestaurants(userId);
-        if (likedRestaurants.length === 0) {
-            res.status(404).json({ message: "No liked restaurants found" });
+    "/",
+    [
+        verifyJWT,
+        check("name", "Name is required").not().isEmpty(),
+        check("owner_id", "Owner id is required").not().isEmpty(),
+        validarCampos
+    ], async (req: Request, res: Response) => {
+        const restaurant: RestaurantModel = req.body;
+        const user: User | undefined = req.user;
+        if (!user) {
+            res.status(500).json({ message: "User not found" });
+            return;
         }
-        res.status(200).json(likedRestaurants);
-    } catch (error) {
-        res.status(500).json({ message: "Error finding liked restaurants by user" });
-    }
-});
+        try {
+            const restaurantSaved = await restaurantRepository.save(restaurant, user);
+            res.status(201).json({
+                message: "Restaurant created successfully",
+                restaurantSaved
+            });
+        } catch (error) {
+            res.status(500).json({ message: "Error creating the restaurant" });
+        }
+    });
 
-// POST api/restaurant/:userId/liked-restaurant/:restaurantId
-restaurantRouter.post("/:userId/liked-restaurant/:restaurantId", async (req: Request, res: Response) => {
-    const userId: number = parseInt(req.params.userId);
-    const restaurantId: number = parseInt(req.params.restaurantId);
-    try {
-        await restaurantRepository.addLikedRestaurant(userId, restaurantId);
-        res.status(200).json({ message: "Liked restaurant added successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Error adding liked restaurant" });
-    }
-});
+
+// GET api/restaurant
+restaurantRouter.get(
+    "/",
+    [
+        verifyJWT,
+    ],
+    async (req: Request, res: Response) => {
+        const user: User | undefined = req.user;
+        try {
+            const restaurants = await restaurantRepository.findByOwner(user?.user_id);
+            res.status(200).json(restaurants);
+        } catch (error) {
+            res.status(500).json({ message: "Error finding restaurants by owner" });
+        }
+    });
+
+
+// GET api/restaurant/favorite-restaurants
+restaurantRouter.get(
+    "/favorite-restaurants",
+    [
+        verifyJWT,
+    ]
+    , async (req: Request, res: Response) => {
+        const user: User | undefined = req.user;
+        try {
+            const favoriteRestaurants = await restaurantRepository.findFavoriteRestaurants(user?.user_id);
+            if (favoriteRestaurants.length === 0) {
+                res.status(404).json({ message: "No favorite restaurants found" });
+            }
+            res.status(200).json(favoriteRestaurants);
+        } catch (error) {
+            res.status(500).json({ message: "Error finding favorite restaurants by user" });
+        }
+    })
+
+// POST api/restaurant/favorite-restaurant/:restaurantId
+restaurantRouter.post(
+    "/favorite-restaurant/:restaurantId",
+    [
+        verifyJWT,
+    ],
+    async (req: Request, res: Response) => {
+        const user: User | undefined = req.user;
+        const restaurantId: number = parseInt(req.params.restaurantId);
+        if (!restaurantId) {
+            res.status(500).json({ message: "Restaurant id is required" });
+            return;
+        }
+        try {
+            await restaurantRepository.addFavoriteRestaurant(user?.user_id, restaurantId);
+            res.status(200).json({ message: "Favorite restaurant added successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Error adding favorite restaurant" });
+        }
+    });
+
+
+// GET api/restaurant/liked-restaurants
+restaurantRouter.get(
+    "/liked-restaurants",
+    [
+        verifyJWT,
+    ],
+    async (req: Request, res: Response) => {
+        const user: User | undefined = req.user;
+        try {
+            const likedRestaurants = await restaurantRepository.findLikedRestaurants(user?.user_id);
+            if (likedRestaurants.length === 0) {
+                res.status(404).json({ message: "No liked restaurants found" });
+            }
+            res.status(200).json(likedRestaurants);
+        } catch (error) {
+            res.status(500).json({ message: "Error finding liked restaurants by user" });
+        }
+    });
+
+// POST api/restaurant/liked-restaurant/:restaurantId
+restaurantRouter.post(
+    "/liked-restaurant/:restaurantId",
+    [
+        verifyJWT,
+    ],
+    async (req: Request, res: Response) => {
+        const user: User | undefined = req.user;
+        const restaurantId: number = parseInt(req.params.restaurantId);
+        if (!restaurantId) {
+            res.status(500).json({ message: "Restaurant id is required" });
+            return;
+        }
+        try {
+            await restaurantRepository.addLikedRestaurant(user?.user_id, restaurantId);
+            res.status(200).json({ message: "Liked restaurant added successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Error adding liked restaurant" });
+        }
+    });
 
 export default restaurantRouter;

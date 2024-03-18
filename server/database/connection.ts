74d0befe3b030360c4877fa617c40e448d8bccaa
Yasai <alexvidalcasado@gmail.com>
2024-03-18 08:32:10 +0100

    [FIX] created database pool

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,34 +1,8 @@
 import mysql from "mysql2";
 import dbConfig from "./db.config";
 
-let connection: mysql.Connection;
+const pool = mysql.createPool(dbConfig);
 
-const handleDisconnect = () => {
-  connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASS,
-    database: dbConfig.NAME,
-  });
-
-  connection.connect((err) => {
-    if (err) {
-      console.log("Error when connecting to db:", err);
-      setTimeout(handleDisconnect, 2000);
-    }
-  });
-
-  connection.on("error", (err) => {
-    console.log("db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-      console.log("Connection lost, reconnecting... 👍");
-    } else {
-      throw err;
-    }
-  });
-};
-
-handleDisconnect();
+const connection = pool;
 
 export default connection;
--- server/database/db.config.ts
+++ server/database/db.config.ts
@@ -1,6 +1,9 @@
 export default {
-  HOST: process.env.DB_HOST ?? "localhost",
-  USER: process.env.DB_USER ?? "root",
-  PASS: process.env.DB_PASS ?? "",
-  NAME: process.env.DB_NAME ?? "cheese",
+  host: process.env.DB_HOST ?? "localhost",
+  user: process.env.DB_USER ?? "root",
+  password: process.env.DB_PASS ?? "",
+  database: process.env.DB_NAME ?? "cheese",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
